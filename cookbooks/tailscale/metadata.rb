5da5a202fb195d0b4b9c9e74ffc81f989542c35d
Gregory Schofield <greg.c.schofield@gmail.com>
2023-01-14 11:03:32 -0500

    First commit
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

---
+++ cookbooks/tailscale/metadata.rb
@@ -0,0 +1,6 @@
+name 'tailscale'
+description 'A cookbook for installing the tailscale client'
+maintainer 'Greg Schofield'
+maintainer_email 'greg.c.schofield@gmail.com'
+license 'Apache-2.0'
+version '1.0.0'
---
+++ cookbooks/tailscale/recipes/default.rb
@@ -0,0 +1,30 @@
+apt_update "update apt"
+
+package "curl"
+
+bash "add tailscale signingkey and repo" do
+  code <<-BASH
+  curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+  curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+  BASH
+  action :nothing
+  notifies :update, "apt_update[update apt]", :immediately
+end
+
+package "tailscale" do
+  notifies :run, "bash[add tailscale signingkey and repo]", :before
+end
+
+service "tailscaled" do
+  action [:start, :enable]
+  notifies :run, "executep[tailscale up]", :immediately
+end
+
+file "#{ENV["HOME"]}/ts.key" do
+  content "tskey-auth-kU3Sm46CNTRL-m3PUKTpYCadBjeLXc27PcdNdvqM9eSQTN"
+end
+
+execute "tailscale up" do
+  command "sudo tailscale up --auth-key file:#{ENV["HOME"]}/ts.key"
+  action :nothing
+end
